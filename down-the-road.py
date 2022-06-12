3ede6ed6ceb8c24bdc7cc14fca659ed3872e97e7
Munchicken <spierce@austin.rr.com>
2022-06-12 12:50:10 -0500

    added some variable practice

--- down-the-road.py
+++ down-the-road.py
@@ -1,6 +1,18 @@
 # Down the Road game
 # by Sarah Pierce
 
+#variable practice
+game_name = 'Down the Road'  #string
+version = 1  #integer
+player_speed = 2.5  #float
+game_over = False  #boolean
+
+print (type(game_name))
+print (type(version))
+print (type(player_speed))
+print (type(game_over))
+
+#input/output practice
 user_name = input('Please enter your name ')
 
 salutation = 'Hello ' + user_name + ', welcome to Down the Road game!'
