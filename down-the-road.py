9b86770a642d9b7e406196c21f0b763712770025
Munchicken <spierce@austin.rr.com>
2022-06-14 17:19:10 -0500

    Made game object oriented

--- down-the-road.py
+++ down-the-road.py
@@ -13,37 +13,55 @@ SCREEN_HEIGHT = 800
 SCREEN_TITLE = 'Down the Road'
 WHITE_COLOR = (255,255,255)
 BLACK_COLOR = (0,0,0)
-TICK_RATE = 60  #FPS
 
 #initialize display
-game_screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-game_screen.fill(WHITE_COLOR)
 clock = pygame.time.Clock()
-pygame.display.set_caption(SCREEN_TITLE)
 
-#load resources
-player_image = pygame.image.load('player.png')
-player_image = pygame.transform.scale(player_image, (50,50))
+#classes
+class Game:
 
-#initialize game variables
-is_game_over = False
+    #constants
+    TICK_RATE = 60  #FPS
 
-#game loop
-while not is_game_over:
-    #event loop
-    for event in pygame.event.get():
-        #check for quit event
-        if event.type == pygame.QUIT:
-            is_game_over = True
-        print(event)
+    #load resources
+    player_image = pygame.image.load('player.png')
+    player_image = pygame.transform.scale(player_image, (50,50))
+
+    #initializer
+    def __init__(self, title, width, height):
+        self.title = title
+        self.width = width
+        self.height = height
         
-    #draw some stuff
-    #pygame.draw.rect(game_screen,BLACK_COLOR,[350,350,100,100])
-    #pygame.draw.circle(game_screen,BLACK_COLOR,(400,300),50)
-    game_screen.blit(player_image, (375,375))
+        #initialize display
+        self.game_screen = pygame.display.set_mode((width,height))
+        self.game_screen.fill(WHITE_COLOR)
+        pygame.display.set_caption(title)
 
-    pygame.display.update()
-    clock.tick(TICK_RATE)
+    #game loop method
+    def run_game_loop(self):
+        #initialize game variables
+        is_game_over = False
+
+        #game loop
+        while not is_game_over:
+            #event loop
+            for event in pygame.event.get():
+                #check for quit event
+                if event.type == pygame.QUIT:
+                    is_game_over = True
+                print(event)
+        
+            #draw some stuff
+            self.game_screen.blit(self.player_image, (375,375))
+            
+            pygame.display.update()  #update display
+            clock.tick(self.TICK_RATE)  #update clock
+
+#create game
+new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game.run_game_loop()
+
 #end game
 pygame.quit()
 quit()
